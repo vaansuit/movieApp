8b63f1c794efe4b1434b918f530307073af733e1
vaansuit <vvansuit@outlook.com>
2023-01-19 20:16:45 -0300

    update: consuming tmdb api

--- lib/app/controller/getMoviesController.dart
+++ lib/app/controller/getMoviesController.dart
@@ -1,21 +1,26 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:movie_app/app/model/movie_model.dart';
 
 class GetMoviesController {
-  final Dio dio;
-  GetMoviesController(this.dio);
+  Future<List<MovieModel>> fetchMovies() async {
+    final dio = Dio();
+    final response = await dio
+        .get('https://api.themoviedb.org/3/discover/movie', queryParameters: {
+      'api_key': '7e165a7d15e9f92360b305bc79dfb897',
+      'language': 'pt-BR',
+      'sort_by': 'popularity.desc',
+      'include_adult': false,
+      'include_video': false,
+      'page': 1,
+      'with_genres': 27,
+      'with_watch_monetization_types': 'flatrate'
+    });
+    final List<dynamic> jsonList = response.data['results'];
 
-  Future<List<MovieModel>> getAllMovies() async {
-    List<MovieModel> movies = [];
-    String baseUrl =
-        'https://api.themoviedb.org/3/discover/movie?api_key=7e165a7d15e9f92360b305bc79dfb897&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=27&with_watch_monetization_types=flatrate';
-
-    final response = await dio.get(baseUrl);
-    movies = List.from(
-      response.data.map((movie) {
-        return MovieModel.fromJson(movie);
-      }),
-    );
-    return movies;
+    return jsonList
+        .map((jsonItem) => MovieModel.fromJson(json.encode(jsonItem)))
+        .toList();
   }
 }
